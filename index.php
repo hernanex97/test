a9f2d237ece4765e7fedf147e3fa5aa4f4bfed06
basanta <basanta@ibm.com>
2021-06-07 19:24:40 -0300

    a

--- index.php
+++ index.php
@@ -68,7 +68,7 @@
                     </div>
                     <div class="col-lg-4">
                         <div class="team-member">
-                            <img class="mx-auto rounded-circle" src="assets/img/team/2.jpg" alt="..." />
+                            <img class="mx-auto rounded-circle" src="assets/img/team/1.jpg" alt="..." />
                             <h4>Rocio Ferreiro Rico</h4>
                             <p class="text-muted">Developer</p>
                             <a class="btn btn-dark btn-social mx-2" href="#!"><i class="fab fa-twitter"></i></a>
@@ -87,9 +87,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-lg-8 mx-auto text-center"><p class="large text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p></div>
-                </div>
+            
             </div>
         </section>
         <!-- Clients-->
