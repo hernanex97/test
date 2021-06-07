48915c620e787dd904c935455cc558d0524bacc9
basanta <basanta@ibm.com>
2021-06-07 19:08:22 -0300

    Update index.php

--- index.php
+++ index.php
@@ -48,40 +48,9 @@
             <div class="container">
                 <div class="masthead-subheading">Welcome to the best Friends chatbot!</div>
                 <div class="masthead-heading text-uppercase">¿Estas preparado?</div>
-                <a class="btn btn-primary btn-xl text-uppercase" href="#services">Quiero chatear ya!</a>
             </div>
         </header>
-        <!-- Services-->
-        <section class="page-section" id="services">
-            <div class="container">
-                <div class="text-center">
-                    
-                </div>
-                <div class="row text-center">
-                    <div class="col-md-4">
-                        <span class="fa-stack fa-4x">
-                            
-                        </span>
-                        
-                    </div>
-                    <div class="col-md-4">
-                        
-                        
-                        <span class="fa-stack fa-4x">
-                       
-                        </span>
-                        
-                        
-                    </div>
-                    <div class="col-md-4">
-                        <span class="fa-stack fa-4x">
-                            
-                        </span>
-                        
-                    </div>
-                </div>
-            </div>
-        </section>
+       
         
         <!-- Team-->
         <section class="page-section bg-light" id="team">
